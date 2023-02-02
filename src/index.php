bcc04e5475b7ba1d006d2134b0f5c53393fd3f9b
Joe Alt <joejoyalt@gmail.com>
2023-02-02 12:26:16 +0100

    Bootstrap first styles

--- src/index.php
+++ src/index.php
@@ -23,19 +23,28 @@ if (isset($_SESSION['logged_in'])) {
 </head>
 
 <body>
-<form action="./php-functions/validate.php" method="post" enctype="multipart/form-data" >
-    <label>Email<br><br> <input type="text" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required></label><br><br>
-    <label>Password<br><br> <input type="password" name="pswd" minlength="8" maxlength="21" required></label><br><br>
-    <?php 
-    
-    ?>
-    <button type="submit" style="cursor: pointer;">Log in</button>
-    <div>
-        <?php
-        if(isset($_GET['err']))
-            echo "Incorrect credentials. Please <a href='index.php'>try again</a>";
-        ?>
+    <div class="row align-items-center text-center">
+        <div class="col-md-auto bg-light">
+            <form action="./php-functions/validate.php" method="post" enctype="multipart/form-data" class="box">
+                <div class="mb-3">
+                    <label for="loginFormEmail" class="form-label">Email address</label>
+                    <input type="email" name="email" class="email-pswd form-control" id="loginFormEmail" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" aria-describedby="emailHelp" required>
+                </div>
+                <div class="mb-3">
+                    <label for="loginFormPassword" class="form-label">Password</label>
+                    <input type="password" name="pswd" class="email-pswd form-control" id="loginFormPassword" minlength="8" maxlength="21" required>
+                </div>
+                <button type="submit" class="log-in btn btn-primary" style="cursor: pointer;">Login</button>
+                <?php
+                if (isset($_GET['err']))
+                    echo "<br>Incorrect credentials. Please <a href='index.php'>try again</a>";
+                ?>
+            </form>
+        </div>
+        <div class="col position-relative p-0">
+            <img src="./assets/img/background.png" class="img-fluid" alt="VPN Img">
+        </div>
     </div>
-</form>
 </body>
+
 </html>
