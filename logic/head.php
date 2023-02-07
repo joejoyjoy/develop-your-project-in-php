38a24ec9f49a5b9c2f9f5c3bec9d94b1afeec4a3
Joe Alt <joejoyalt@gmail.com>
2023-02-07 17:34:26 +0100

    vpnConnection js connected

--- logic/head.php
+++ logic/head.php
@@ -13,7 +13,7 @@
     <script defer src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
     <script defer src='./preview.js'></script>
-    <script defer src='./vpnConnect.js'></script>
+    <script defer src='vpnConnecting.js'></script>
 
 
     <title>VPN server</title>
